f38757709b4774f089caa26c799b006d617b9c44
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-06-21 11:06:32 +0100

    made some design changes and changed the 'create playlist' logic to be a reducer function

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 
 import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 
@@ -11,79 +11,50 @@ import { Options } from '../components/Options'
 import { Playlists } from '../components/List'
 
 import { fetchSuggestedSongs } from '../functions/fetchData'
-import { createNewPlaylist } from '../functions/library'
+import { createNewPlaylist, playlistDispatch } from '../functions/library'
 import { displayMessage } from '../functions/prompt'
 
 const Playlist = () => {
+  const { data: genres, isFetching, error } = useGetTopGenresQuery()
+
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   const [params, setParams] = useSearchParams()
 
-  const { data: genres, isFetching, error } = useGetTopGenresQuery()
-
   const { playlists, favorites, blacklist } = useSelector( (state) => state.library )
 
-  const [genreNum, setGenreNum] = useState(5);
-  const [playlistInfo, setPlaylistInfo] = useState({name: '', img: '', genres: [], tracks: []})
-  const [suggestedSongs, setSuggestedSongs] = useState([])
-
-  const handleChange = (e) => {
-    const id = e.target.getAttribute('id')
-    setPlaylistInfo( (prev) => ({...prev, [id]: e.target.value}) )
-  }
+  const [newPlaylist, setNewPlaylist] = useReducer(playlistDispatch, {genreNum: 5, playlistInfo: {name: '', img: '', genres: [], tracks: []}, suggestedSongs: [], genreAction: {action: '', id: null}})
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(playlistInfo.name !== "") {
-      createNewPlaylist(dispatch, playlistInfo)
-      setParams({})
+    if(newPlaylist.playlistInfo.name !== "") {
+      createNewPlaylist(dispatch, newPlaylist.playlistInfo)
       displayMessage(dispatch, 'New playlist created!')
-    
-      setPlaylistInfo({img: '', name: '', tracks: [], genres: []})
-      setSuggestedSongs([])
-      setParams({})
+      
+      setNewPlaylist({type: 'RESET'})
+      navigate('/playlists')
+    } else {
+      displayMessage(dispatch, 'Song title is empty.')
     }
   }
 
-  const suggestSongs = (genreid) => {
-    fetchSuggestedSongs(genreid, setSuggestedSongs, blacklist, favorites)
-  }
+  useEffect(() => {
 
-  const removeSuggestedSongs = (genreid) => {
-    setSuggestedSongs( prev => {
-      const tracks = [...prev]
-      const newTracks = tracks.filter( song => song.genreid !== genreid )
-      return newTracks;
-    })
-  }
+    if(newPlaylist.genreAction === 'remove') {
+      const id = newPlaylist.genreAction.id
+      setNewPlaylist({type: 'REMOVESUGGESTEDSONG', payload: id})
+    }
+    if(newPlaylist.genreAction === 'add') {
+      const id = newPlaylist.genreAction.id
+      const suggestedSongs = newPlaylist.suggestedSongs
+      fetchSuggestedSongs({id, setNewPlaylist, suggestedSongs, blacklist , favorites})
+    }
 
-  const handleGenre = (genre, action) => {
-    setPlaylistInfo( prev => {
-      let genres = [...prev.genres]
-      if(action === 'add') {
-        suggestSongs(genre.id)
-        genres.push(genre)
-      } else {
-        removeSuggestedSongs(genre.id)
-        genres = genres.filter( elem => elem.id !== genre.id )
-      }
-      return {...prev, genres}
-    })
-  }
-
-  const addSong = (song) => {
-    setPlaylistInfo( prev => ({...prev, tracks: [song, ...prev.tracks]}) )
-  }
-
-  const removeSong = (songid) => {
-    const tracks = playlistInfo.tracks
-    const newTracks = tracks.filter( track => track.id !== songid )
-    setPlaylistInfo( prev => ({...prev, tracks: newTracks}) )
-  }
+  }, [newPlaylist.genreAction])
 
   return (
-    <div className={`p-6 lg:pt-[80px] relative overflow-x-clip ${!params.get('add') === 'true' ? 'overflow-y-hidden' : ''} h-[100vh]`}>
+    <div className={`p-2 mg:p-4 pt-[80px] relative overflow-x-clip ${!params.get('add') === 'true' ? 'overflow-y-hidden' : ''} h-[100vh]`}>
       <div className={`w-full h-full transition-transform ${params.get('add') === 'true' && 'translate-x-[-110%]'}`}>
         <div className="w-full flex justify-between items-center mb-4">
           <h3 className="font-bold text-white text-xl">Playlists</h3>
@@ -98,20 +69,18 @@ const Playlist = () => {
           <Playlists playlists={playlists} />
         }
       </div>
+      
       <CreatePlaylist
         handleSubmit={handleSubmit}
         params={params}
-        playlistInfo={playlistInfo}
-        handleChange={handleChange}
+        playlistInfo={newPlaylist.playlistInfo}
+        setNewPlaylist={setNewPlaylist}
+        handleChange={(e) => setNewPlaylist({type: 'HANDLECHANGE', id: e.target.id, payload: e.target.value})}
         genres={genres}
-        handleGenre={handleGenre}
         isFetching={isFetching}
         error={error}
-        genreNum={genreNum}
-        setGenreNum={setGenreNum}
-        suggestedSongs={suggestedSongs}
-        removeSong={removeSong}
-        addSong={addSong}
+        genreNum={newPlaylist.genreNum}
+        suggestedSongs={newPlaylist.suggestedSongs}
       />
     </div>
   )
