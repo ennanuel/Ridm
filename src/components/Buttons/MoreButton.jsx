f0630e4d209ed2837f52df38c30f5f2b2d3983b1
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-06-21 10:42:41 +0100

    made some design changes and added a click event handler to this component

--- src/components/Buttons/MoreButton.jsx
+++ src/components/Buttons/MoreButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const MoreButton = ({ setParams }) => {
+const MoreButton = ({ setLimit, limit, length }) => {
   return (
+    (limit && length > 20) &&
     <div className="w-full flex justify-center my-[50px]">
-      <button className="text-gray-300 border-2 border-gray-300 rounded-md font-semibold px-3 py-1 m-auto transition-[background-color] hover:bg-gray-300 hover:text-black">
+      <button onClick={() => setLimit(false)} className="text-gray-300 border-2 border-gray-300 rounded-md font-semibold px-3 py-1 m-auto transition-[background-color] hover:bg-gray-300 hover:text-black">
           See more
       </button>
     </div>
