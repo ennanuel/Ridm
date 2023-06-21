2c29d2e77b4c41fc11fb4507eebcccab009a5e68
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-06-21 11:04:00 +0100

    made little changes to button tag7

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,11 @@
 import { useSelector } from 'react-redux'
 
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { Library } from '../components/List'
 
 const Favorites = () => {
   const { favorites, blacklist } = useSelector( state => state.library )
-  const navigate = useNavigate()
 
   return (
     <div className="p-4 flex flex-col h-[100vh]">
@@ -18,12 +17,11 @@ const Favorites = () => {
         <Library library={favorites} favorites={favorites} blacklist={blacklist} /> :
         <div className="p-4 flex-1 flex flex-col items-center justify-center gap-4">
           <h3 className="text-gray-400 font-bold text-xl">You haven't liked anything yet.</h3>
-          <button 
-            onClick={() => navigate('/')}
+          <Link to="/"
             className="px-4 py-2 rounded-md border-2 border-gray-400 text-gray-400 text-sm font-bold transition-[background-color] hover:bg-gray-400 hover:text-black"
           >
             Go Home
-          </button>
+          </Link>
         </div>
       }
     </div>
