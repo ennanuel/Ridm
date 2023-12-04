ed42f735ca09036ff33d75919fc77650dec80f43
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-12-04 20:35:43 +0100

    Fixed options modal and added new design changes

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -8,7 +8,7 @@ import { filterOptions } from "../../utils/option";
 import Option from './Option';
 import OptionBtn from "./OptionBtn";
 
-const Options = ({type, small, song, artist, genre, album, radio, playlist, tracks, i, favorite, blacklist }) => {
+const Options = ({ type, small, song, artist, genre, album, radio, playlist, tracks, i, favorite, blacklist }) => {
     const filteredOptions = useMemo(() => filterOptions({ options, type, favorite, blacklist }), [blacklist, favorite, playlist]);
     const iconSize = useMemo(() => window.innerWidth < 1024 ? 18 : small ? 20 : 30, []);
     const [showModal, setShowModal] = useState(false);
@@ -19,50 +19,44 @@ const Options = ({type, small, song, artist, genre, album, radio, playlist, trac
 
     const navigate = useNavigate();
 
-    function handleOption (cbAction, values) {
+    function handleOption(cbAction, values) {
         const navigateTo = cbAction(values);
         if (navigateTo) navigate(navigateTo);
         setShowModal(false);
     }
-    function open () {
+    function open() {
         if (!btnRef.current) return;
         const rect = btnRef.current.getBoundingClientRect();
         setShowModal(true);
         setPosition({ height: window.innerHeight / 2, width: window.innerWidth / 2, clientX: rect.x, clientY: rect.y });
     }
-    function close () {
+    function close() {
         setShowModal(false);
     }
 
     return (
         <div className="relative" ref={modalRef}>
-            {
-                showModal &&
-                <>
-                    <div onClick={close} className="fixed z-[99999] top-0 left-0 backdrop-blur-sm bg-black/30 h-full w-full lg:w-[calc(100vw-300px)]" />
-                    <ul className={`${modalPosition} absolute animate-slowfade shadow-lg overflow-hidden shadow-black/60 z-[999999] w-max flex flex-col border text-gray-200 text-sm font-semibold border-white/10 rounded-md bg-black/80 backdrop-blur-lg`}>
-                        {
-                            filteredOptions
-                            .map( (option, num) => 
-                                <Option 
-                                    key={num} 
-                                    option={option} 
-                                    handleOption={handleOption} 
-                                    song={song} 
-                                    artist={artist} 
-                                    album={album}
-                                    genre={genre} 
-                                    playlist={playlist} 
-                                    radio={radio} 
-                                    tracks={tracks} 
-                                    i={i} 
-                                /> 
-                            )
-                        }
-                    </ul>
-                </>
-            }
             <OptionBtn open={open} btnRef={btnRef} iconSize={iconSize} optionType={type} />
+            <ul className={`${modalPosition} ${showModal ? 'hover:pointer-events-auto hover:flex' : 'hover:pointer-events-none hover:hidden' } hidden pointer-events-none peer-focus:flex peer-focus:pointer-events-auto absolute z-1 animate-slowfade shadow-lg overflow-hidden shadow-black/60 z-[999999] w-max flex-col border text-gray-200 text-sm font-semibold border-white/10 rounded-md bg-black/80 backdrop-blur-lg`}>
+                {
+                    filteredOptions
+                        .map((option, num) =>
+                            <Option
+                                key={num}
+                                option={option}
+                                handleOption={handleOption}
+                                song={song}
+                                artist={artist}
+                                album={album}
+                                genre={genre}
+                                playlist={playlist}
+                                radio={radio}
+                                tracks={tracks}
+                                i={i}
+                            />
+                        )
+                }
+            </ul>
         </div>
     )
 }
