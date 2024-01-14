d5bddec8636b9362eca6c1f5dbb570607ffdf784
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-14 09:05:33 +0100

    Refactored code

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,37 +1,38 @@
-import { useEffect, useReducer, useState } from 'react'
+import { useEffect, useMemo, useReducer, useState } from 'react'
 
-import { Link, useNavigate, useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
-import { useSelector } from 'react-redux'
+import { useGetTopGenresQuery } from '../redux/services/DeezerApi';
 
-import { useGetTopGenresQuery } from '../redux/services/DeezerApi'
-
-import CreatePlaylist from '../components/CreatePlaylist'
-import { Options } from '../components/Options'
-import { Playlists } from '../components/List'
+import CreatePlaylist from '../components/CreatePlaylist';
 
 import { fetchSuggestedSongs } from '../utils/fetchData'
 import { createNewPlaylist, playlistDispatch, playlistState } from '../utils/library'
-import { displayMessage } from '../utils/prompt'
+import PlaylistsFront from '../components/PlaylistsFront'
 
 const Playlist = () => {
-  const { playlists } = useSelector((state) => state.library);
   const { data: genres, isFetching, error } = useGetTopGenresQuery();
   const navigate = useNavigate();
   const [params, setParams] = useSearchParams();
   const [newPlaylist, setNewPlaylist] = useReducer(playlistDispatch, playlistState);
+  const isInAddPage = useMemo(() => params.get('add') === 'true', [params]);
+  const [errorSavingPlaylist, setErrorSavingPlaylist] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    const { playlistInfo } = newPlaylist;
-    if(playlistInfo.name) {
-      createNewPlaylist(playlistInfo);
-      displayMessage('New playlist created!');
+    try {
+      const { playlistInfo } = newPlaylist;
+      await createNewPlaylist(playlistInfo);
       setNewPlaylist({ type: 'RESET' });
       navigate('/playlists');
-    } else {
-      displayMessage('Song title is empty.');
+    } catch (error) {
+      setErrorSavingPlaylist(true);
     }
+  }
+
+  const handleChange = (e) => {
+    setNewPlaylist({ type: 'HANDLECHANGE', id: e.target.id, payload: e.target.value });
+    setErrorSavingPlaylist(setErrorSavingPlaylist(false));
   }
 
   useEffect(() => { 
@@ -40,54 +41,39 @@ const Playlist = () => {
   }, [genres])
 
   useEffect(() => {
-    if(newPlaylist.genreAction === 'remove') {
+    if (newPlaylist.genreAction === 'remove') {
       const id = newPlaylist.genreAction.id
-      setNewPlaylist({type: 'REMOVESUGGESTEDSONG', payload: id})
+      setNewPlaylist({ type: 'REMOVESUGGESTEDSONG', payload: id })
     }
-
-    if(newPlaylist.genreAction === 'add') {
+    if (newPlaylist.genreAction === 'add') {
       const id = newPlaylist.genreAction.id;
-      const suggestedSongsIds = newPlaylist.suggestedSongs.map( song => song.id );
+      const suggestedSongsIds = newPlaylist.suggestedSongs.map(song => song.id);
       fetchSuggestedSongs({ id, setNewPlaylist, suggestedSongsIds })
         .then(songs => setNewPlaylist({ type: 'ADDSUGGESTEDSONG', payload: songs }))
         .catch((error) => console.error(error));
     }
-
   }, [newPlaylist.genreAction])
 
   useEffect(() => {
-    const text = `Ridm - ${params.get('add') == 'true' ? 'Create New Playlist' : 'Playlists'}`
+    const text = `Ridm - ${isInAddPage ? 'Create New Playlist' : 'Playlists'}`
     document.getElementById('site_title').innerText = text
   }, [params])
 
   return (
-    <div className={`p-2 flex md:p-4 overflow-x-clip ${!params.get('add') === 'true' ? 'overflow-y-hidden' : ''}`}>
-      <div className={`min-w-full transition-transform ${params.get('add') === 'true' && 'translate-x-[-110%]'}`}>
-        <div className="w-full flex justify-between items-center mb-4">
-          <h3 className="font-bold text-white text-xl">Playlists</h3>
-          <Options type="playlists" navigate={navigate} />
-        </div>
-        {
-          playlists.length < 1 ? 
-            <div className="mt-[-40px] flex flex-col items-center justify-center gap-4 h-[90vh]">
-              <h3 className="text-gray-400 font-bold text-xl">You don't have any saved playlists</h3>
-              <Link to="/playlists?add=true" className="flex items-center justify-center font-bold text-xs md:text-sm bg-gray-400 text-black border border-gray-400 px-3 h-[28px] md:h-[36px] rounded-[18px] hover:text-gray-400 hover:bg-transparent transition-[background-color]">Create New</Link>
-            </div> :
-          <Playlists playlists={playlists} />
-        }
-      </div>
-      
+    <div className={`p-2 flex md:p-4 overflow-x-clip ${!isInAddPage ? 'overflow-y-hidden' : ''}`}>
+      <PlaylistsFront isInAddPage={isInAddPage} />
       <CreatePlaylist
         handleSubmit={handleSubmit}
-        params={params}
+        isInAddPage={isInAddPage}
         playlistInfo={newPlaylist.playlistInfo}
         setNewPlaylist={setNewPlaylist}
-        handleChange={(e) => setNewPlaylist({type: 'HANDLECHANGE', id: e.target.id, payload: e.target.value})}
+        handleChange={handleChange}
         genres={genres}
         isFetching={isFetching}
         error={error}
         genreNum={newPlaylist.genreNum}
         suggestedSongs={newPlaylist.suggestedSongs}
+        errorSavingPlaylist={errorSavingPlaylist}
       />
     </div>
   )
