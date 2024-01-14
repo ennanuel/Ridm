03cd6ef2b34166ea64e07e3e1536bb530ee00867
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-14 09:07:32 +0100

    Refactored 'createNewPlaylist' and 'dipatchActions' functions to improve site performance

--- src/utils/library.js
+++ src/utils/library.js
@@ -3,12 +3,20 @@ import { hidePrompt } from "../redux/features/promptSlice";
 import { displayMessage } from "./prompt";
 import { store } from '../redux/store';
 
-export const createNewPlaylist = (data) => {
-  const playlistId = data.name + Math.floor(Math.random() * 100000);
-  store.dispatch(createPlaylist({ ...data, playlistId }));
-  displayMessage('Playlist created!');
-  store.dispatch(setLibraryStorage());
-}
+export const createNewPlaylist = (data) => new Promise(
+  function (resolve, reject) {
+    if (data?.name) {
+      const playlistId = Date.now();
+      store.dispatch(createPlaylist({ ...data, id: playlistId }));
+      displayMessage('Playlist created!');
+      store.dispatch(setLibraryStorage());
+      resolve();
+    } else {
+      displayMessage('Couldn\'t create playlist!');
+      reject();
+    }
+  }
+);
 
 export const editCurrentPlaylist = (values) => {
   store.dispatch(editPlaylist(values));
@@ -53,44 +61,47 @@ export const removeBlacklist = (type, id) => {
 
 export const playlistState = {
   genreNum: 5,
-  playlistInfo: { name: '', img: '', genres: [], tracks: [] },
+  playlistInfo: { id: '', name: '', img: '', genres: [], tracks: [] },
   suggestedSongs: [], genreAction: { action: '', id: null }
+};
+  
+const dispatchActions = {
+  moregenres: ({ state }) => ({ ...state, genreNum: null }),
+  lessgenres: ({ state }) => ({ ...state, genreNum: 5 }),
+  handlechange: ({ state, action }) => ({ ...state, playlistInfo: { ...state.playlistInfo, [action.id]: action.payload } }),
+  reset: () => playlistState,
+  setgenres: ({ state, action }) => ({ ...state, genres: action.payload }),
+  addgenre: ({ state, action }) => ({
+    ...state,
+    genreAction: 'add', id: action.payload.id,
+    playlistInfo: { ...state.playlistInfo, genres: [...state.playlistInfo.genres, action.payload] }
+  }),
+  removegenre: ({ state, action }) => ({
+    ...state,
+    genreAction: { action: 'remove', id: action.payload.id },
+    playlistInfo: {
+      ...state.playlistInfo,
+      genres: state.playlistInfo.genres.filter(elem => elem.id !== action.payload)
+    }
+  }),
+  addsong: ({ state, action }) => ({
+    ...state,
+    playlistInfo: { ...state.playlistInfo, tracks: [...state.playlistInfo.tracks, action.payload] }
+  }),
+  removesong: ({ state, action }) => ({
+    ...state,
+    playlistInfo: {
+      ...state.playlistInfo,
+      tracks: state.playlistInfo.tracks.filter(track => track.id !== action.payload.id)
+    }
+  }),
+  addsuggestedsong: ({ state, action }) => ({ ...state, suggestedSongs: [...state.suggestedSongs, ...action.payload] }),
+  removesuggestedsong: ({ state, action }) => ({ ...state, suggestedSongs: state.suggestedSongs.filter(song => song.genreid !== action.payload) }),
 };
 
 export const playlistDispatch = (state, action) => {
-  const dispatchActions = {
-    moregenres:  {...state, genreNum: null},
-    lessgenres: { ...state, genreNum: 5 },
-    handlechange: { ...state, playlistInfo: { ...state.playlistInfo, [action.id]: action.payload } },
-    reset: playlistState,
-    setgenres: { ...state, genres: action.payload },
-    addgenre: {
-      ...state,
-      genreAction: 'add', id: action.payload.id,
-      playlistInfo: { ...state.playlistInfo, genres: [...state.playlistInfo.genres, action.payload] }
-    },
-    removegenre: {
-      ...state,
-      genreAction: { action: 'remove', id: action.payload.id },
-      playlistInfo: {
-        ...state.playlistInfo,
-        genres: state.playlistInfo.genres.filter(elem => elem.id !== action.payload)
-      }
-    },
-    addsong: {
-      ...state,
-      playlistInfo: { ...state.playlistInfo, tracks: [...state.playlistInfo.tracks, action.payload] }
-    },
-    removesong: {
-      ...state,
-      playlistInfo: {
-        ...state.playlistInfo,
-        tracks: state.playlistInfo.tracks.filter(track => track.id !== action.payload.id)
-      }
-    },
-    addsuggestedsong: { ...state, suggestedSongs: [...state.suggestedSongs, ...action.payload] },
-    removesuggestedsong: { ...state, suggestedSongs: state.suggestedSongs.filter(song => song.genreid !== action.payload) },
-  }
   const actionType = action.type.toLowerCase();
-  return dispatchActions[actionType] || state;
+  const dispatchAction = dispatchActions[actionType]
+  if (!dispatchAction) return state;
+  return dispatchAction({ state, action });
 }
