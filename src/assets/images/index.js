d8d6b2e2d5d87bc2ddc13247acd102260150534c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-14 08:57:45 +0100

    Added new images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,17 @@
 import loader from './loader.svg';
 import logo from './ridm_icon_plain.svg';
 import logo2 from './ridm_icon_gradient.svg';
+import songImage from './song.svg';
+import artistImage from './artist.svg';
+import albumImage from './album.svg';
+import radioImage from './radio.svg';
 
 export {
   logo,
   loader,
-  logo2
+  logo2,
+  songImage,
+  artistImage,
+  radioImage,
+  albumImage
 };
