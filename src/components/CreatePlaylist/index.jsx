90d24fe3634c09afda9de5fac68e8aff0c06e3b7
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-14 09:00:42 +0100

    Made a few code refactoring

--- src/components/CreatePlaylist/index.jsx
+++ src/components/CreatePlaylist/index.jsx
@@ -4,30 +4,40 @@ import PlaylistGenres from './PlaylistGenres'
 import Details from './Details'
 import Header from './Header'
 
-const index = ({ setNewPlaylist, handleSubmit, params, playlistInfo, handleChange, genres, isLoading, error, genreNum, suggestedSongs, removeSong, addSong}) => {
+const index = ({ setNewPlaylist, handleSubmit, playlistInfo, handleChange, genres, isLoading, error, genreNum, suggestedSongs, removeSong, errorSavingPlaylist, isInAddPage }) => {
   return (
-    <form onSubmit={handleSubmit} className={`min-w-full min-h-[90vh] transition-[transform,opacity] ${params.get('add') === 'true' ? 'translate-x-[-100%]' : 'opacity-0 pointer-events-none h-0'} p-3`}>
+    <form onSubmit={handleSubmit} className={`min-w-full min-h-[90vh] transition-[transform,opacity] ${isInAddPage ? 'translate-x-[-100%]' : 'opacity-0 pointer-events-none h-0'} p-3`}>
       <Header />
-
-      <Details playlistInfo={playlistInfo} handleChange={handleChange} />
-      
-      <PlaylistGenres 
-        playlistGenres={playlistInfo?.genres || []} 
+      <Details
+        errorSavingPlaylist={errorSavingPlaylist}
+        playlistInfo={playlistInfo}
+        handleChange={handleChange}
+      />
+      <PlaylistGenres
+        playlistGenres={playlistInfo?.genres || []}
         genres={genres?.data || []}
-        isLoading={isLoading} 
-        error={error} 
-        genreNum={genreNum} 
+        isLoading={isLoading}
+        error={error}
+        genreNum={genreNum}
         setNewPlaylist={setNewPlaylist}
       />
 
       <div className="w-full grid md:grid-cols-2 grid-cols-1 gap-6 mt-4">
         {
           suggestedSongs.length > 0 &&
-          <Recommended suggestedSongs={suggestedSongs} setNewPlaylist={setNewPlaylist} tracks={playlistInfo.tracks} />
+          <Recommended
+            suggestedSongs={suggestedSongs}
+            setNewPlaylist={setNewPlaylist}
+            tracks={playlistInfo.tracks}
+          />
         }
         {
           playlistInfo.tracks.length > 0 &&
-          <PlaylistSongs tracks={playlistInfo.tracks} setNewPlaylist={setNewPlaylist} removeSong={removeSong} />
+          <PlaylistSongs
+            tracks={playlistInfo.tracks}
+            setNewPlaylist={setNewPlaylist}
+            removeSong={removeSong}
+          />
         }
       </div>
     </form>
