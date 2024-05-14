a84382da6300af7e0cd10106cf2cc0597d41a7a0
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-14 18:04:38 +0100

    Updated logic for displaying 'Add or remove item from playlist

--- src/utils/option.js
+++ src/utils/option.js
@@ -1,13 +1,11 @@
-import { store } from "../redux/store";
 
-export const filterOptions = ({ options, type, favorite, blacklist, small }) => {
+export const filterOptions = ({ options, type, favorite, blacklist, isInPlaylistPath }) => {
     function filterOutOptions(option) {
         const isFavoriteOrNot = option.name !== (favorite ? 'Add to favorites' : 'Remove from favorites');
-        const inPlaylistOrNot = option.name !== (playlists ? 'Add to playlist' : 'Remove from playlist');
+        const inPlaylistOrNot = option.name !== (isInPlaylistPath ? 'Add to playlist' : 'Remove from playlist');
         const inBlacklistOrNot = (blacklist ? !/Don't show/i.test(option.name) : option.name !== 'Remove from blacklist');
         return isFavoriteOrNot && inPlaylistOrNot && inBlacklistOrNot
     };
-    var { library: { playlists } } = store.getState();
     const optionsType = options[type];
     const filteredOptions = optionsType.filter(filterOutOptions)
     return filteredOptions
