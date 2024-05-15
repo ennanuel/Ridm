3a6aea800ce6685bcc356800620a06b8e7009261
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:44:55 +0100

    Made changes to 'border-radius'

--- src/components/NavigationAndSearch/Navigations.jsx
+++ src/components/NavigationAndSearch/Navigations.jsx
@@ -8,8 +8,8 @@ const Navigations = ({ scrolled }) => {
   const goFront = useMemo(() => () => navigate(+1), []);
   return (
     <>
-    <button onClick={goBack} className={`h-[30px] aspect-square hidden lg:flex items-center justify-center rounded-md text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 ${scrolled ? 'bg-white/5' : 'bg-black/80'}`}><BsChevronLeft size={20} /></button>
-    <button onClick={goFront} className={`h-[30px] aspect-square hidden lg:flex items-center justify-center rounded-md text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 ${scrolled ? 'bg-white/5' : 'bg-black/80'}`}><BsChevronRight size={20} /></button>
+    <button onClick={goBack} className={`h-[40px] aspect-square hidden lg:flex items-center justify-center rounded-[20px] text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 ${scrolled ? 'bg-white/5' : 'bg-black/80'}`}><BsChevronLeft size={20} /></button>
+    <button onClick={goFront} className={`h-[40px] aspect-square hidden lg:flex items-center justify-center rounded-[20px] text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 ${scrolled ? 'bg-white/5' : 'bg-black/80'}`}><BsChevronRight size={20} /></button>
     </>
   )
 }
