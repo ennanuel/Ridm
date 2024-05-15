e4da0667828eb7ce26424f8323c440f5fbe36d43
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:47:48 +0100

    Made changes to the icon and it's size, and also added 'small' property

--- src/components/Options/OptionBtn.jsx
+++ src/components/Options/OptionBtn.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BiMenuAltLeft } from 'react-icons/bi';
-import { BsThreeDots } from 'react-icons/bs';
+import { BiDotsVerticalRounded, BiMenuAltLeft } from 'react-icons/bi';
 
-const OptionBtn = ({ open, optionType, btnRef, iconSize }) => {
+const OptionBtn = ({ open, optionType, btnRef, small }) => {
     return (
         <button
             onClick={open}
             ref={btnRef}
-            className={`text-white flex items-center justify-center drop-shadow-lg peer`}
+            className={`text-white aspect-square rounded-full ${!small && 'w-[40px] h-[40px] md:w-[50px] md:h-[50px] rounded-full bg-white/5 hover:bg-white/10'} flex items-center justify-center drop-shadow-lg peer`}
         >
             {
                 optionType == 'playlists' ?
-                    <BiMenuAltLeft size={iconSize} /> :
-                    <BsThreeDots size={iconSize} />
+                    <BiMenuAltLeft size={small ? 18 : 25} /> :
+                    <BiDotsVerticalRounded size={small ? 18 : 25} />
             }
         </button>
     )
