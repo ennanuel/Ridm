6775ce2352c0be894746728f8adfa534b7cf1f98
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:30:27 +0100

    added 'max-width' property for text

--- src/components/Cards/SongBar/SongInfo.jsx
+++ src/components/Cards/SongBar/SongInfo.jsx
@@ -7,7 +7,7 @@ const SongInfo = ({ song, artistId }) => {
   return (
     <div className="flex-1 flex flex-col justify-center mx-3">
       <Link to={`/songs/${song.id}`}>
-        <p className="text-xs md:text-sm font-semibold text-white truncate max-w-[70%]">{song?.title}</p>
+        <p className="text-xs md:text-sm font-semibold text-white truncate max-w-[200px]">{song?.title}</p>
       </Link>
         <div className="flex flex-row flex-wrap gap-1 items-center text-gray-300">
           {
@@ -20,7 +20,7 @@ const SongInfo = ({ song, artistId }) => {
               </Link>
             )) :
             <Link to={`/artists/${song.artist.id}`}>
-              <p className="text-[0.75em] font-semibold text-gray-300 flex flex-row items-center justify-start gap-1 truncate">
+              <p className="text-[0.75em] font-semibold text-gray-300 max-w-[200px] flex flex-row items-center justify-start gap-1 truncate">
                 {song?.artist.name}
               </p>
             </Link>
@@ -29,7 +29,7 @@ const SongInfo = ({ song, artistId }) => {
         </div>
       {
         song?.album?.title && 
-        <p className="text-xs text-gray-500 font-semibold truncate max-w-[70%]">
+        <p className="text-xs text-gray-500 font-semibold truncate max-w-[200px]">
           {song.album.title}
         </p>
       }
