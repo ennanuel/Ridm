20fe02cf05ebd80b2ea0698a35393a695aa80a72
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:20:55 +0100

    Made changes to dynamic background color

--- src/components/Buttons/FavoriteButton.jsx
+++ src/components/Buttons/FavoriteButton.jsx
@@ -13,13 +13,15 @@ const FavoriteButton = ({ data, type, small, text }) => {
                         addFavorites(type, data)
                 )
             }
-            style={{ color: text }}
-            className={'flex items-center justify-center text-white drop-shadow-lg drop-shadow-black/50 transition-transform active:scale-90'}
+            style={{
+                backgroundColor: text ? text.replace(')', ',.8') : ''
+            }}
+            className={`flex items-center justify-center ${small ? 'text-white drop-shadow-lg drop-shadow-black/50' : 'text-white md:w-[50px] md:h-[50px] w-[40px] h-[40px] bg-white/5'} transition-transform active:scale-90 rounded-full`}
         >
             {
                 data?.favorite ?
-                    <AiFillHeart size={small ? 20 : 40} /> :
-                    <AiOutlineHeart size={small ? 20 : 40} />
+                    <AiFillHeart size={small ? 20 : 25} /> :
+                    <AiOutlineHeart size={small ? 20 : 25} />
             }
         </button>
     )
