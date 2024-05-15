d542265c942770b7e558785601fb80779136fc40
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:32:39 +0100

    modified 'border-radius' property of artist's image

--- src/components/Cards/ArtistCard.jsx
+++ src/components/Cards/ArtistCard.jsx
@@ -4,8 +4,8 @@ const ArtistCard = ({ artist, i }) => {
   const navigate = useNavigate()
   
   return (
-    <div style={{'--delay': i/20 + 's'}} className="artist-card flex flex-col p-2 rounded-sm cursor-pointer transition-[background-color] hover:bg-white/5" onClick={() => { navigate(`/artists/${artist.id}`) }}>
-      <img alt="artist" src={artist?.picture_medium} className="w-full aspect-square rounded-lg shadow-md shadow-black/50" />
+    <div style={{'--delay': i/20 + 's'}} className="artist-card flex flex-col p-2 md:p-3 rounded-xl cursor-pointer transition-[background-color] hover:bg-white/5" onClick={() => { navigate(`/artists/${artist.id}`) }}>
+      <img alt="artist" src={artist?.picture_medium} className="w-full aspect-square rounded-full" />
       <p className="mt-3 font-bold text-[0.7em] text-gray-400 text-left truncate">{artist.type}</p>
       <p className="font-semibold text-sm text-white text-left truncate">{artist.name}</p>
     </div>
