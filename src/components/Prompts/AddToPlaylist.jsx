1899e29efbcfbd9902b4fd27dbeaed1c63ce3009
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:49:15 +0100

    Made styling modifications to modal and it's buttons

--- src/components/Prompts/AddToPlaylist.jsx
+++ src/components/Prompts/AddToPlaylist.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 import { editPlaylist } from '../../redux/features/librarySlice'
 import { hidePlaylistPrompt, hideSuccessMessage, showSuccessMessage } from '../../redux/features/promptSlice'
+import { MdAdd } from 'react-icons/md'
 
 const AddToPlaylist = () => {
   const { displayPlaylistPrompt, songsToAdd } = useSelector( state => state.prompt )
@@ -25,17 +26,21 @@ const AddToPlaylist = () => {
 
   return (
     <section className={`fixed z-[100] top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm flex items-center justify-center transition-opacity ${!displayPlaylistPrompt && 'pointer-events-none opacity-0'}`}>
-      <div className={`w-full max-w-[250px] overflow-hidden bg-black/90 border border-white/10 rounded-sm transition-transform ${!displayPlaylistPrompt && 'scale-50'}`}>
-        <p className="p-3 font-semibold text-center text-gray-200 text-base">Add to playlist</p>
-          <ul className="flex flex-col border-t border-white/10 text-sm text-gray-400">
-            <li onClick={() => {navigate('/playlists?add=true'); closePrompt()}} className="flex items-center justify-start py-2 px-4 border-b font-bold hover:bg-white/5 hover:text-gray-200 border-white/5">Create new playlist</li>
-              {
-                  playlists.map( (playlist, i) => 
-                    <li key={playlist.id} onClick={() => saveToPlaylist(playlist)} className="flex items-center justify-start py-2 px-4 border-b hover:bg-white/5 hover:text-gray-200 border-white/5">{playlist.name}</li>
-                  )
-              }
-          </ul>
-        <button onClick={closePrompt} className="w-full py-2 border-t-2 border-white/10 bg-red-800 hover:bg-red-600 text-sm text-gray-200 font-semibold">Cancel</button>
+      <div className={`w-full flex flex-col gap-4 max-w-[260px] overflow-hidden bg-black/90 border border-white/5 p-4 rounded-[20px] transition-transform ${!displayPlaylistPrompt && 'scale-50'}`}>
+        <p className="font-semibold text-center text-gray-200 text-base">Add to playlist</p>
+        <ul className="mt-4 flex flex-col gap-2 text-sm text-gray-400">
+          {
+            playlists?.length < 1 ?
+              <li onClick={() => { navigate('/playlists?add=true'); closePrompt() }} className="flex rounded-[10px] border border-white/5 items-center gap-2 justify-start h-[40px] px-4 font-bold hover:text-gray-200 hover:border-transparent hover:bg-white/5">
+                <MdAdd size={25} />
+                <span>New playlist</span>
+              </li> :
+              playlists.map((playlist, i) =>
+                <li key={playlist.id} onClick={() => saveToPlaylist(playlist)} className="flex items-center justify-start rounded-[10px] border border-white/5 h-[40px] px-4 border-b hover:text-gray-200 hover:border-transparent hover:bg-white/5">{playlist.name}</li>
+              )
+          }
+        </ul>
+        <button onClick={closePrompt} className="w-full h-[40px] rounded-[10px] flex items-center justify-center bg-red-800 hover:bg-red-600 text-sm text-gray-200 font-semibold">Cancel</button>
       </div>
     </section>
   )
