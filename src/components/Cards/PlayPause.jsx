f104b20bbb46ddc90360c4450231cc4f0be77489
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:34:51 +0100

    Made styling modfications for button

--- src/components/Cards/PlayPause.jsx
+++ src/components/Cards/PlayPause.jsx
@@ -1,23 +1,21 @@
 import { FaPause, FaPlay } from 'react-icons/fa'
 
-const PlayPause = ({ handlePause, handlePlay, isCurrent, isPlaying, size, cover, spacing }) => (
-  isPlaying && isCurrent ? (
-    <div className={`play_pause ${cover && 'p-[20px] pl-[22px] bg-[#91D8F7] shadow'} pause_play_icon flex flex-row justify-center items-center rounded-lg transition-transform hover:scale-110 shadow-lg shadow-black/50`}>
+const PlayPause = ({ handlePause, handlePlay, isCurrent, isPlaying, size, cover }) => (
+  isPlaying && isCurrent ?
+    <div className={`play_pause ${cover && 'p-[20px] pl-[22px] bg-[#91D8F7] shadow'} pause_play_icon flex justify-center items-center rounded-[15px] transition-transform hover:scale-110 shadow-lg shadow-black/50`}>
       <FaPause
         size={size || 15}
         className={`${cover ? "text-black" : "text-[#91D8F7]"}`}
-        onClick={handlePause} 
+        onClick={handlePause}
       />
-    </div>
-  ) : (
-    <div className={`play_pause ${cover && `p-[${spacing ? spacing + 'px' : '20px'}] pl-[${spacing ? (spacing + 2) + 'px' : '22px'}] bg-[#91D8F7] shadow`} pause_play_icon flex flex-row justify-center items-center rounded-lg transition-transform hover:scale-110 shadow-lg shadow-black/50`}>
+    </div> :
+    <div className={`play_pause ${cover && `h-[50px] w-[50px] bg-black border pl-[2px] border-white/5`} pause_play_icon flex justify-center items-center rounded-full transition-transform hover:scale-110`}>
       <FaPlay
         size={size || 15}
-        className={`${cover ? "text-black" : "text-[#91D8F7]"}`}
+        className={`${cover ? "text-white" : "text-[#91D8F7]"}`}
         onClick={handlePlay}
       />
     </div>
-  )
 );
 
 export default PlayPause;
