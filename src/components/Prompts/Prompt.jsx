548fcf05186f41a12ef96cef98f3adc873cd5b75
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:50:22 +0100

    made styling changes to modal and action buttons

--- src/components/Prompts/Prompt.jsx
+++ src/components/Prompts/Prompt.jsx
@@ -18,18 +18,18 @@ const Prompt = () => {
   function closePrompt() { dispatch(hidePrompt()) };
 
   return (
-    <section className={`fixed z-[100] top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm flex items-center justify-center transition-opacity ${!displayPrompt && 'pointer-events-none opacity-0'}`}>
-      <div className={`w-full max-w-[300px] overflow-hidden bg-black border border-white/10 rounded-sm transition-transform ${!displayPrompt && 'scale-50'}`}>
-        <p className="p-5 font-semibold text-center text-gray-200 text-sm md:text-base">{promptMessage}</p>
-        <div className="w-full flex text-gray-200 text-xs md:text-sm">
+    <section className={`fixed z-[100] top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm flex items-center transition-opacity ${!displayPrompt && 'pointer-events-none opacity-0'}`}>
+      <div className={`flex-1 w-full m-auto flex flex-col gap-4 max-w-[300px] overflow-hidden bg-black border border-white/5 p-2 rounded-[20px] transition-transform ${!displayPrompt && 'scale-50'}`}>
+        <p className="px-4 py-2 font-semibold text-center text-gray-200 text-sm md:text-base">{promptMessage}</p>
+        <div className="w-full flex justify-stretch gap-2 text-gray-200 text-xs md:text-sm">
             <button 
-              className="py-2 flex-1 border-t border-white/10 hover:bg-white/5" 
+              className="px-4 flex-1 h-[40px] flex justify-center items-center rounded-[10px] bg-white/5 hover:bg-white/10 text-sm" 
               onClick={executePrompt}
             >
               {acceptText}
             </button>
             <button 
-              className="py-2 flex-1 border-l border-white/10 bg-red-800 hover:bg-red-600 text-sm md:text-base" 
+              className="px-4 flex-1 h-[40px] flex justify-center items-center rounded-[10px] bg-red-800 hover:bg-red-600 text-sm" 
               onClick={closePrompt}
             >
               {cancelText}
