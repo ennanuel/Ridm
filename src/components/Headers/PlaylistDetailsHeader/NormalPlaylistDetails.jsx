e2132a778f6b7745d8acc91ac6f9c8196a382807
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:37:55 +0100

    modified some styling and refactored code

--- src/components/Headers/PlaylistDetailsHeader/NormalPlaylistDetails.jsx
+++ src/components/Headers/PlaylistDetailsHeader/NormalPlaylistDetails.jsx
@@ -4,16 +4,16 @@ import { PlayButton, ShuffleButton } from "../../Buttons"
 
 const NormalPlaylistDetails = ({ playlist, primary, secondary }) => {
   return (
-    <div className="flex-col flex-1">        
-      <h2 style={{color: primary}} className="text-white font-bold text-2xl">{playlist.name}</h2>
-      <div className="flex flex-wrap items-center font-semibold text-xs mb-2 gap-1">
-        <p style={{ color: primary }} className="text-gray-400 text-sm">{playlist.tracks.length} songs</p>
-        <ul style={{ color: primary }} className="flex flex-wrap items-center text-gray-500">
+    <div className="flex-1">
+      <h2 className="text-white font-bold text-[2.3rem]">{playlist.name}</h2>
+      <div className="flex flex-wrap items-center font-semibold text-xs mb-4 gap-1">
+        <p className="text-gray-400 text-sm">{playlist.tracks.length} songs</p>
+        <ul className="flex flex-wrap items-center text-gray-500">
           {
-            playlist.genres.map( (genre, i) =>
+            playlist.genres.map((genre, i) =>
               <>
-              <BsDot size={16} />
-              <li key={i}>{genre.name}</li>
+                <BsDot size={16} />
+                <li key={i}>{genre.name}</li>
               </>
             )
           }
@@ -21,12 +21,12 @@ const NormalPlaylistDetails = ({ playlist, primary, secondary }) => {
       </div>
       <div className="flex flex-row gap-4 flex-wrap justify-between items-center">
         <div className="flex-1 flex flex-row gap-4">
-          <PlayButton tracks={playlist.tracks} song={playlist.tracks[0]} i={0} />
-          <ShuffleButton tracks={playlist.tracks} />
+          <PlayButton bg={primary} text={secondary} tracks={playlist.tracks} song={playlist.tracks[0]} i={0} />
+          <ShuffleButton bg={secondary} text={primary} tracks={playlist.tracks} />
         </div>
-          <Options type="playlist" song={playlist.tracks[0]} tracks={playlist.tracks} i={0} playlist={playlist} />
+        <Options type="playlist" song={playlist.tracks[0]} tracks={playlist.tracks} i={0} playlist={playlist} />
       </div>
-      </div>
+    </div>
   )
 }
 
