2bbc61132d1de602f8f88a730f41c3683414e1d7
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-15 09:23:13 +0100

    Made styling changes and modified logic for dynamic colors

--- src/components/Buttons/PlayButton.jsx
+++ src/components/Buttons/PlayButton.jsx
@@ -9,9 +9,13 @@ const PlayButton = ({ song, i, tracks, album, bg, text }) => {
 
   return (
     <button
-      style={{ backgroundColor: bg, color: text }}
+      style={{
+        backgroundColor: bg,
+        color: text,
+        boxShadow: text ? `0 0 2px 0 ${text.replace(')', ',.6)')}` : '0 3px 6px rgba(0, 0, 0, 0.1), 0 3px 12px rgba(0, 0, 0, 0.05)'
+      }}
       onClick={handlePlay}
-      className="md:h-[50px] text-xs md:text-sm lg:text-base h-[40px] md:min-w-[120px] truncate px-4 rounded-md border border-[rgb(145,216,247,0)] bg-[rgba(145,216,247,1)] text-[#000] shadow shadow-black/50 font-semibold relative flex flex-row justify-center items-center gap-1 opacity-90 transition-[transform] active:scale-90 hover:opacity-100"
+      className="md:h-[50px] text-xs md:text-sm lg:text-base h-[40px] md:min-w-[120px] truncate px-4 rounded-[15px] bg-[rgb(145,216,247,1)] text-[#000] shadow-lg shadow-black/50 font-semibold relative flex flex-row justify-center items-center gap-1 opacity-90 transition-[transform] active:scale-90 hover:opacity-100"
     >
       <span><BsPlayFill size={25} /></span>
       <span>Play</span>
