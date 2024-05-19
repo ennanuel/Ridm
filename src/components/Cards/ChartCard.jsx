e24ba0c4bd302352a15b907c4eb64c0a0bbb9674
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-19 16:01:32 +0100

    Modified border radius of card

--- src/components/Cards/ChartCard.jsx
+++ src/components/Cards/ChartCard.jsx
@@ -6,9 +6,10 @@ const ChartCard = ({ active, type, category, i, handleClick }) => {
     return (
         <div 
             style={{'--delay': i/10 + 's'}}
-            className={`group relative overflow-hidden border-2 px-4 py-2 h-full cursor-pointer rounded-xl flex flex-col items-end justify-end bg-white/5 hover:bg-black/30 transition-[opacity,transform] duration-300 border-transparent ${active ? 'opacity-0 scale-[2] border-transparent' : 'hover:border-white/5 shaodw-lg shadow-black/30'} ${origin}`} 
+            className={`group relative overflow-hidden border-2 px-4 py-2 h-full cursor-pointer rounded-[20px] flex flex-col items-end justify-end bg-white/5 hover:bg-black/30 transition-[opacity,transform] duration-300 border-transparent ${active ? 'opacity-0 scale-[2] border-transparent' : 'hover:border-white/5 shaodw-lg shadow-black/30'} ${origin}`} 
             onClick={() => ((type === null || type === '') || !active) && handleClick(category.to)}
         >
+            {/* {!active && <img src={category.image} className="absolute top-0 left-[-5px] w-full h-full scale-[1.1] transition-[transform,opacity] group-hover:opacity-10 group-hover:scale-[1.2] object-cover opacity-50" alt={category.name} />} */}
             <div className="relative group-hover:translate-y-[0px] translate-y-[30px] transition-transform cat-text overflow-hidden z-[1] text-right">
                 <p className={`md:mb-4 w-fit transition-transform duration-500 text-white text-2xl md:text-[3em] font-bold capitalize ${active ? 'translate-y-[100%]' : 'translate-y-[0] delay-200'}`}>{category.name}</p>
             </div>
