1f3b0d906415bcc754f8bc6a8d8ff64a8fccfea4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:18:10 +0100

    added padding

--- src/components/Sidebar/Logo.jsx
+++ src/components/Sidebar/Logo.jsx
@@ -2,7 +2,7 @@ import { logo } from "../../assets/images"
 
 const Logo = () => {
   return (
-    <div className="w-full flex flex-row gap-[5px] lg:justify-start justify-center items-center">
+    <div className="w-full flex flex-row gap-[5px] px-2 lg:justify-start justify-center items-center">
         <img src={logo} alt="logo" className="h-[30px] md:h-[40px]"/>
         <h2 className="w-max text-white font-semibold text-lg md:text-xl lg:text-2xl">Ridm.</h2>
     </div>
