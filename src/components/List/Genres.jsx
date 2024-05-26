dfb4cda13fa4599cba320cca816c1e83e3b43242
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:37:28 +0100

    Refactored code

--- src/components/List/Genres.jsx
+++ src/components/List/Genres.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import { GenreCard } from '../Cards';
 import { GenreLoading, Error } from '../LoadersAndError';
 import { getData } from '../../utils/getData';
@@ -6,29 +6,25 @@ import { useSelector } from 'react-redux';
 
 const Genres = ({ children, genres, isFetching, error, noFilter }) => {
   const library = useSelector(state => state.library);
-  const [newGenres, setNewGenres] = useState([])
-
-  useEffect(() => {
-    setNewGenres(getData({type: 'genres', data: genres, noFilter}))
-  }, [library, genres, noFilter])
+  const newGenres = useMemo(() => getData({ type: 'genres', data: genres, noFilter }), [library, genres, noFilter]);
 
   return (
     <>
       <h2 className="text-white font-bold text-2xl mb-4">{children}</h2>
       {
         isFetching ?
-        <GenreLoading num={10} /> :
-        (
-          error ?
-          <Error title="Could not load genres" /> :
-          <div className="grid grid-cols-2 md:grid-cols-4 items-center justify-center gap-6">
-            {
-              newGenres?.map( (genre, i) => (
-                <GenreCard key={i} genre={genre} i={i} />
-              ))
-            }
-          </div>
-        )
+          <GenreLoading num={10} /> :
+          (
+            error ?
+              <Error title="Could not load genres" /> :
+              <div id="genres" className="grid grid-cols-2 md:grid-cols-4 items-center justify-center gap-6">
+                {
+                  newGenres?.map((genre, i) => (
+                    <GenreCard key={i} genre={genre} i={i} />
+                  ))
+                }
+              </div>
+          )
       }
     </>
   )
