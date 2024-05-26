9bd8d0ffe301dee24028078c5a9445f908ab9d65
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 19:02:58 +0100

    Changed layout and design of card component

--- src/components/Cards/PlaylistCard.jsx
+++ src/components/Cards/PlaylistCard.jsx
@@ -3,24 +3,15 @@ import { Options } from "../Options"
 
 const PlaylistCard = ({ i, playlist }) => {
   return (
-    <div style={{animationDelay: i / 20 + 's', animationFillMode: 'forwards'}} className="relative animate-slideleft opacity-0">
+    <div style={{ animationDelay: i / 20 + 's', animationFillMode: 'forwards' }} className="animate-slideleft opacity-0 p-3 rounded-[15px] bg-white/5 hover:bg-white/10 flex flex-col gap-3">
       <Link to={`/playlists/${playlist.id}`}>
-        <div className="relative bg-white/10 rounded-md h-[200px] overflow-hidden shadow-lg shadow-black/50">
-          {
-            playlist.tracks.length > 0 &&
-            <img 
-              src={playlist.tracks[0].album.cover_medium} 
-              className="absolute w-full h-full object-cover top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" 
-              alt="" 
-            />
-          }
-          <div className="relative h-full w-full px-4 py-2 bg-black/70 flex flex-col items-start justify-end">
-            <span className="text-gray-400 text-xs font-semibold">playlist</span>
-            <span className="text-white text-base font-semibold truncate w-full block">{playlist.name}</span>
-          </div>
-        </div>
+        <img src={playlist?.tracks[0]?.album.cover_medium} alt="" className="h-[150px] rounded-[10px] w-full bg-white/5 object-cover" />
       </Link>
-      <div className="absolute z-[1] top-2 right-2">
+      <div className="flex justify-between items-center gap-4">
+        <Link to={`/playlists/${playlist.id}`} className="flex flex-col">
+          <p className="font-bold text-lg text-white">{playlist.name}</p>
+          <p className="text-xs text-gray-400">{playlist.tracks.length} {playlist.tracks.length === 1 ? 'track' : 'tracks'}</p>
+        </Link>
         <Options tracks={playlist.tracks} song={playlist.tracks[0]} i={0} playlist={playlist} type={'playlist'} small={true} />
       </div>
     </div>
