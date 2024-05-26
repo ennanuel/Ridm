1ed3ac568cbb7c5738a6cf9f6fbf5163fd6ecda6
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 19:12:03 +0100

    Made styling changes

--- src/components/Cards/RadioCard/Card.jsx
+++ src/components/Cards/RadioCard/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Card = ({ show, radio }) => {
   return (
-    <div>
-      <img className={`shadow-lg shadow-black/50 ${show && 'w-[150px]'}`} src={radio.picture_medium} alt="" />
-      <p className="flex flex-col gap-1 mt-2 font-bold mx-2">
+    <div className="flex flex-col gap-3">
+      <img className={`shadow shadow-black/50 ${show && 'w-[150px]'} aspect-square`} src={radio.picture_medium} alt="" />
+      <p className="flex flex-col font-bold">
         <span className="text-xs text-gray-400">{radio.type}</span>
         <span className="text-white truncate font-semibold">{radio.title}</span>
       </p>
