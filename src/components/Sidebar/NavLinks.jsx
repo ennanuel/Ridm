a3e2703895eea186911e1c15e28b189cc0063edd
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:17:27 +0100

    Made design and layout changes to the sidebar component

--- src/components/Sidebar/NavLinks.jsx
+++ src/components/Sidebar/NavLinks.jsx
@@ -7,32 +7,33 @@ import { links, secondLinks } from "../../assets/data/constants";
 
 
 const NavLinks = () => {
-    const location = useLocation()
+    const { pathname } = useLocation()
 
     return (
-        <nav className="row-span-2 sticky top-0 left-0 lg:flex hidden flex-col py-10 px-5 h-[100vh] bg-[#151515] border-r border-white/5">
-            <Logo />
-            <div className="flex flex-col justify-between h-full align-start">
-                <ul className="mt-10 flex flex-col gap-1">
+        <div className="row-span-2 sticky top-2 left-0 z-[10] lg:block hidden h-[calc(100vh-16px)]">
+            <nav className="h-full flex flex-col gap-4 pt-10 pb-4 px-2 bg-[#151515bd] rounded-[15px] border border-white/5 shadow shadow-black/30">
+                <Logo />
+                <ul className="mt-6 flex-1 flex flex-col gap-1">
                     {
                         links.map(item => (
                             <Links
-                                active={((new RegExp(item.to, 'i')).test(location.pathname || '') && item.to !== '/') || ((!(/(genres|charts|playlist|favorites)/i).test(location.pathname)) && item.to === '/')}
+                                active={((new RegExp(item.to, 'i')).test(pathname || '') && item.to !== '/') || ((!(/(genres|charts|playlist|favorites)/i).test(pathname)) && item.to === '/')}
                                 link={item}
                             />
-                        )
-                        )
+                        ))
                     }
                 </ul>
-                <ul className="flex flex-col gap-2 border-y border-white/10 pt-4 pb-5">
-                    <h2 className="font-semibold text-lg text-white mb-2">Your Library</h2>
-                    {
-                        secondLinks.map((secondLink, i) => <SecondLinks key={i} link={secondLink} active={(new RegExp(secondLink.to, 'i')).test(location.pathname)} />)
-                    }
-                </ul>
+                <div className="flex flex-col gap-4 border rounded-[20px] border-white/10 p-2 bg-black/20">
+                    <h2 className="font-semibold text-lg text-white px-2 border-b border-white/5 p-2">Your Library</h2>
+                    <ul className="flex flex-col gap-2">
+                        {
+                            secondLinks.map((secondLink, i) => <SecondLinks key={i} link={secondLink} active={(new RegExp(secondLink.to, 'i')).test(pathname)} />)
+                        }
+                    </ul>
+                </div>
                 <Footer />
-            </div>
-        </nav>
+            </nav>
+        </div>
     )
 };
 
