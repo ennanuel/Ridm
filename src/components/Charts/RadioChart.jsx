7e8e6e3907b5560cc0d743cc3b8f87b3cbefbe4c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:56:48 +0100

    stopped passing 'favorites' and 'blacklist' as parameters for 'Radios' component

--- src/components/Charts/RadioChart.jsx
+++ src/components/Charts/RadioChart.jsx
@@ -5,13 +5,13 @@ import { useGetTopRadiosQuery } from "../../redux/services/DeezerApi"
 import { Radios } from "../List"
 import { MoreButton } from "../Buttons"
 
-const RadioChart = ({ blacklist, favorites }) => {
+const RadioChart = () => {
   const [limit, setLimit] = useState(true)
   const {data, isFetching, error} = useGetTopRadiosQuery(50)
 
   return (
     <>
-    <Radios blacklist={blacklist} favorites={favorites} radios={data?.data?.slice(0, limit ? 20 : 50)} isFetching={isFetching} error={error} />
+    <Radios radios={data?.data?.slice(0, limit ? 20 : 50)} isFetching={isFetching} error={error} />
     <MoreButton limit={limit} setLimit={setLimit} length={data?.data?.length} />
     </>
   )
