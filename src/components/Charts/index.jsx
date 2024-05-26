a2b71c277026effdba59556fb0671ec438145f66
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:59:10 +0100

    stopped passing 'favorites' and 'blacklist' as parameters for some components, and reordered back link

--- src/components/Charts/index.jsx
+++ src/components/Charts/index.jsx
@@ -1,41 +1,38 @@
-import ArtistChart from "./ArtistChart"
-import SongChart from "./SongChart"
-import AlbumChart from "./AlbumChart"
-import RadioChart from "./RadioChart"
+import ArtistChart from "./ArtistChart";
+import SongChart from "./SongChart";
+import AlbumChart from "./AlbumChart";
+import RadioChart from "./RadioChart";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import { MdArrowBackIos } from "react-icons/md"
-import { useSelector } from "react-redux"
+import { MdArrowBack } from "react-icons/md";
 
 const index = ({ category, active, i }) => {
-    const { blacklist, favorites } = useSelector( state => state.library )
     const origin = i === 0 ? 'origin-top-left' : i === 1 ? 'origin-top-right' : i === 2 ? 'origin-bottom-left' : i === 3 ? 'origin-bottom-right' : ''
     
     return (
         <div 
             className={`w-full h-full duration-300 transition-[opacity,transform] ${active ? '' : 'scale-50 opacity-0 pointer-events-none h-0'} ${origin}`}
         >
-            <div className="flex flex-row justify-between items-center mb-2">
-                <div className="overflow-hidden text-white text-xl font-bold lowercase">
-                    <p className={`transition-transform duration-500 ${active ? 'translate-y-0 delay-200 ' : 'translate-y-[100%]'}`}>{category.name}s</p>
+            <div className="mb-2">
+                <div className="overflow-hidden text-white font-bold lowercase flex items-center gap-2">
+                    <Link to="/charts" className="hover:text-white relative z-1 text-gray-300 flex items-center justify-center">
+                        <MdArrowBack size={20} />
+                    </Link>
+                    <h2 className={`transition-transform duration-500 ${active ? 'translate-y-0 delay-200 ' : 'translate-y-[100%]'} text-xl`}>{category.name}s</h2>
                 </div>
-                <Link to="/charts" className="lg:hidden hover:text-white relative z-1 text-gray-300 flex items-center justify-center text-xs">
-                    <MdArrowBackIos size={12} />
-                    <span>Back</span>
-                </Link>
             </div>
             {
                 active && 
                     (
                         category.name === 'artist' ?
-                        <ArtistChart blacklist={blacklist} favorites={favorites} /> :
+                        <ArtistChart /> :
                         category.name === 'song' ?
-                        <SongChart blacklist={blacklist} favorites={favorites} /> :
+                        <SongChart /> :
                         category.name === 'album' ?
-                        <AlbumChart blacklist={blacklist} favorites={favorites} /> :
+                        <AlbumChart /> :
                         category.name === 'radio' ?
-                        <RadioChart blacklist={blacklist} favorites={favorites} /> :
+                        <RadioChart /> :
                         null
                     )
             }
