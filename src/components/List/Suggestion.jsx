3186a249a8f88863438952ebdcf3e1ea93226f5c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:30:56 +0100

    Refactored code, made some design modifications and icreased the number or radio tracks to display in the suggestion grid

--- src/components/List/Suggestion.jsx
+++ src/components/List/Suggestion.jsx
@@ -4,8 +4,10 @@ import { Error } from "../LoadersAndError"
 import Songs from "./Songs"
 import { useMemo } from "react"
 
+const MOCK_RADIO_SONGS = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
 const Suggestion = ({ radioTracks, radio, songs, isFetching, error, blacklist, favorites }) => {
-    const tracks = useMemo(() => radioTracks?.slice(0, 8), [radioTracks]);
+    const tracks = useMemo(() => radioTracks?.slice(0, 15), [radioTracks]);
 
     function addToQueue() {
         playNext({ tracks });
@@ -17,29 +19,27 @@ const Suggestion = ({ radioTracks, radio, songs, isFetching, error, blacklist, f
         <div>
             <h3 className="mt-4 text-white text-xl font-bold">Suggestions</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-14 lg:gap-6 mt-4">
-                <div>
-                    <div className="grid gap-2 md:gap-4 grid-cols-5 grid-rows-3">
-                        {
-                            isFetching ?
-                                [1, 1, 1, 1, 1, 1, 1, 1, 1].map((elem, i) => (
-                                    <div className={`aspect-square rounded-xl ${(i === 0 || i === 2) && 'col-span-2 row-span-2'} bg-black/50 loading-animation`}></div>
-                                )) :
-                                <>
-                                    <div
-                                        onClick={addToQueue}
-                                        className="col-span-2 row-span-2 relative rounded-xl overflow-hidden aspect-square flex flex-col justify-center items-center font-bold text-3xl bg-black/50 text-white p-2"
-                                    >
-                                        <img src={radio?.picture_medium} alt="" className="absolute w-full h-full blur-md" />
-                                        <p className="relative">Top Mix</p>
-                                    </div>
-                                    {
-                                        tracks?.map(
-                                            (song, i) => <SuggestedCard key={i} song={song} i={i} tracks={tracks} />
-                                        )
-                                    }
-                                </>
-                        }
-                    </div>
+                <div className="grid gap-2 md:gap-4 grid-cols-5 grid-rows-3 aspect-square">
+                    {
+                        isFetching ?
+                            MOCK_RADIO_SONGS.map((elem, i, arr) => (
+                                <div className={`aspect-square rounded-xl ${(i === 0 || i === 2 || i === 10) && 'col-span-2 row-span-2'} bg-black/50 loading-animation`}></div>
+                            )) :
+                            <>
+                                <div
+                                    onClick={addToQueue}
+                                    className="col-span-2 row-span-2 relative rounded-xl overflow-hidden aspect-square flex flex-col justify-center items-center font-bold text-3xl bg-black/50 text-white p-2"
+                                >
+                                    <img src={radio?.picture_medium} alt="" className="absolute w-full h-full blur-md" />
+                                    <p className="relative">Top Mix</p>
+                                </div>
+                                {
+                                    tracks?.map(
+                                        (song, i) => <SuggestedCard key={i} song={song} i={i} tracks={tracks} />
+                                    )
+                                }
+                            </>
+                    }
                 </div>
                 <Songs
                     blacklist={blacklist}
@@ -49,7 +49,7 @@ const Suggestion = ({ radioTracks, radio, songs, isFetching, error, blacklist, f
                     songs={songs}
                     full={true}
                     suggestion={true}
-                >Popular Songs</Songs>
+                />
             </div>
         </div>
     )
