f775e4101fe2048f4cc0b9fdfdf40864792854c6
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:54:04 +0100

    stopped passing 'favorites' and 'blacklist' as parameters for 'Songs' component

--- src/components/Charts/SongChart.jsx
+++ src/components/Charts/SongChart.jsx
@@ -6,14 +6,14 @@ import { Songs } from "../List"
 import { MoreButton } from "../Buttons"
 import { useSearchParams } from "react-router-dom"
 
-const SongChart = ({ blacklist, favorites }) => {
+const SongChart = () => {
   const [limit, setLimit] = useState(true)
   const [params, getParams] = useSearchParams()
   const {data, isFetching, error} = useGetTopChartTracksQuery( params.get('genre') || 0 )
 
   return (
     <>
-    <Songs blacklist={blacklist} favorites={favorites} isFetching={isFetching} error={error} songs={data?.data?.slice(0, limit ? 20 : 50)} />
+    <Songs isFetching={isFetching} error={error} songs={data?.data?.slice(0, limit ? 20 : 50)} />
     <MoreButton limit={limit} setLimit={setLimit} length={data?.data?.length} />
     </>
   )
