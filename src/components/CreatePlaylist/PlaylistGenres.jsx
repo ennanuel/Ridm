fccf8f8343d5546c8e616a9772e9c1199dabb379
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:48:21 +0100

    refactored code and modified styling of 'genre' component

--- src/components/CreatePlaylist/PlaylistGenres.jsx
+++ src/components/CreatePlaylist/PlaylistGenres.jsx
@@ -1,44 +1,44 @@
-import { MdAdd } from "react-icons/md"
+import { MdClose } from "react-icons/md"
 
 const PlaylistGenres = ({ playlistGenres, setNewPlaylist, genres, isLoading, error, genreNum }) => {
     return (
         <>
             <h2 className="text-white text-lg font-bold mt-4">Genres</h2>
             <div className="flex flex-row flex-wrap gap-3 mt-2">
-            {
-                playlistGenres.map( (genre, i) =>
-                <button 
-                    onClick={() => setNewPlaylist({payload: genre.id, type: 'REMOVEGENRE'})} 
-                    key={i} type="button" 
-                    className="px-2 py-1 pr-1 rounded-sm bg-gray-400 text-black text-sm flex items-center justify-center gap-1"
-                >
-                    <span>{genre.name}</span>
-                    <span className="flex items-center justify-center rotate-45"><MdAdd /></span>
-                </button>)
-            }
-            {
-                !(isLoading && error) &&
-                genres.slice(0, genreNum || 10000000).filter(genre => playlistGenres.every( elem => elem.id !== genre.id )).map( (genre, i) =>
-                <button 
-                    onClick={() => setNewPlaylist({payload: genre, type: 'ADDGENRE'})} 
-                    key={i} type="button" 
-                    style={{animationDelay: (i/100 + 's'), animationFillMode: 'forwards'}} 
-                    className="animate-slideleft px-2 py-1 rounded-sm bg-white/5 text-gray-200 opacity-0 text-sm hover:bg-white/10"
-                >
-                    {genre.name}
-                </button>
-                )
-            }  
-            {
-                genres?.length > 0 &&
-                <button 
-                    type="button"
-                    onClick={() => setNewPlaylist({type: genreNum === 5 ? 'MOREGENRES' : 'LESSGENRES'}) } 
-                    className="text-gray-400 text-sm font-bold"
-                >
-                    { genreNum === 5 ? 'more' : 'less' }
-                </button>
-            }
+                {
+                    playlistGenres.map((genre, i) =>
+                        <button
+                            onClick={() => setNewPlaylist({ payload: genre.id, type: 'REMOVEGENRE' })}
+                            key={i} type="button"
+                            className="px-3 h-[30px] pr-2 rounded-md bg-gray-400 text-black text-sm flex items-center justify-center gap-1"
+                        >
+                            <span>{genre.name}</span>
+                            <MdClose />
+                        </button>)
+                }
+                {
+                    !(isLoading && error) &&
+                    genres.slice(0, genreNum || 10000000).filter(genre => playlistGenres.every(elem => elem.id !== genre.id)).map((genre, i) =>
+                        <button
+                            onClick={() => setNewPlaylist({ payload: genre, type: 'ADDGENRE' })}
+                            key={i} type="button"
+                            style={{ animationDelay: (i / 100 + 's'), animationFillMode: 'forwards' }}
+                            className="animate-slideleft px-3 h-[30px] pr-2 rounded-md flex items-center justify-center bg-white/5 text-gray-200 opacity-0 text-sm hover:bg-white/10"
+                        >
+                            {genre.name}
+                        </button>
+                    )
+                }
+                {
+                    genres?.length > 0 &&
+                    <button
+                        type="button"
+                        onClick={() => setNewPlaylist({ type: genreNum === 5 ? 'MOREGENRES' : 'LESSGENRES' })}
+                        className="text-gray-400 text-sm font-bold px-3 h-[30px] pr-2 rounded-md flex items-center justify-center"
+                    >
+                        {genreNum === 5 ? 'more' : 'less'}
+                    </button>
+                }
             </div>
         </>
     )
