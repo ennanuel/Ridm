03464102713f69baa898dbf4188eba192765cc8a
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:17:54 +0100

    Made design and layout changes

--- src/components/Sidebar/MobileNavLinks.jsx
+++ src/components/Sidebar/MobileNavLinks.jsx
@@ -9,10 +9,10 @@ import { links, secondLinks } from "../../assets/data/constants";
 const MobileNavLinks = () => {
     const location = useLocation()
     return (
-        <div className="lg:hidden flex flex-col gap-2 sticky bottom-0 left-0 z-[9999] w-full p-0">
+        <div className="lg:hidden flex flex-col fixed bottom-0 left-0 z-[9999] w-full p-0">
             <MusicPlayer forMobile={true} />
-            <nav className="relative z-[10]  w-full h-[70px] border-t border-gray-500/50 bg-[#151515]">
-                <ul className="w-full h-full flex flex-row items-center justify-evenly md:justify-center gap-3 text-xs">
+            <nav className="relative z-[10] border h-[70px] border-t border-white/5 bg-black/5 backdrop-blur-lg m-2 rounded-[15px]">
+                <ul className="w-full h-full flex flex-row items-center justify-evenly md:justify-center gap-3 text-xs px-2">
                     {
                         links.map(item => (
                             <Links
