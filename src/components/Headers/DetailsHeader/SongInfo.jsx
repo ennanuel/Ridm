e6d206a1788e03fef3da20fbcf3ec97f838fc76f
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:41:10 +0100

    reordered some icons

--- src/components/Headers/DetailsHeader/SongInfo.jsx
+++ src/components/Headers/DetailsHeader/SongInfo.jsx
@@ -7,17 +7,20 @@ const SongInfo = ({ data }) => {
     <>
       {
         data?.type == 'artist' &&
-        <span>
+        <>
+          <BsDot size={20} />
+          <span>
             {
-              data?.nb_fan / 1000000 > 1 ? `${(data?.nb_fan /1000000).toFixed(1)}M ` :
+              data?.nb_fan / 1000000 > 1 ? `${(data?.nb_fan / 1000000).toFixed(1)}M ` :
                 data?.nb_fan / 1000 > 1 ? `${(data?.nb_fan / 1000).toFixed(1)}K ` :
                   ` ${data?.nb_fan} `
             } fans
-        </span>
+          </span>
+        </>
       }
       {
         (data?.type != 'artist') &&
-        <span className="flex flex-row items-center justify-center"><BsDot size={20} /><MdExplicit size={20} /></span>
+        <span className="flex flex-row items-center justify-center"><MdExplicit size={20} /></span>
       }
     </>
   )
