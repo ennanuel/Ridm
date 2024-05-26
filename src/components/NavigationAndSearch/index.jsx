b2bcdd894e3b28fa3c8817b3c5f715d75c2b967f
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:21:52 +0100

    Changed event listener to be for 'bodyRef' instead of windows, also made some layout and styling changes

--- src/components/NavigationAndSearch/index.jsx
+++ src/components/NavigationAndSearch/index.jsx
@@ -3,32 +3,38 @@ import Logo from '../Sidebar/Logo'
 import Searchbar from './Searchbar'
 import Navigations from './Navigations'
 import MusicPlayer from '../MusicPlayer'
+import { useSelector } from 'react-redux'
 
-const NavigationAndSearch = () => {
-  const [{ scrolled, scrolledUp }, setScroll] = useState({ scrolled: false, scrolledUp: false });
+const NavigationAndSearch = ({ bodyRef }) => {
+  const { nowPlaying } = useSelector((state) => state.player);
+  const [{ scrolled, scrolledUp }, setScroll] = useState({ scrolled: false, scrolledUp: false, scrollTop: 0 });
 
-  const handleScroll = () => {
+  const handleScroll = (event) => {
     setScroll(
       (prev) => ({
-        scrolled: window.scrollY > 50,
-        scrolledUp: prev.scrollY < window.scrollY
+        scrolled: event.target.scrollTop > 50,
+        scrollTop: event.target.scrollTop,
+        scrolledUp: prev.scrollTop < event.target.scrollTop
       })
     );
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    if (!bodyRef) return;
+    bodyRef.addEventListener('scroll', handleScroll);
+    return () => bodyRef.removeEventListener('scroll', handleScroll);
+  }, [bodyRef]);
   
   return (
-    <div className={`flex flex-row items-center justify-between gap-5 z-10 top-0 left-0 sticky transition-[background-color,transform] ${scrolled ? 'bg-[#151515]' : 'bg-transparent'} ${scrolledUp && 'translate-y-[-100%] lg:translate-y-[0]'}`}>
-      <div className="relative flex flex-1 items-center justify-between lg:justify-start gap-2 mx-2 md:mx-4">
-        <div className="lg:hidden">
-          <Logo />
+    <div className="top-0 z-[999] sticky flex justify-between gap-2 p-2">
+      <div className={`flex flex-1 flex-row items-center justify-between gap-5 transition-[background-color,transform] py-2 border ${nowPlaying && 'opacity-0 pointer-events-none'} ${scrolled ? 'bg-transparent backdrop-blur-lg border-white/5' : 'bg-transparent border-transparent lg:border-white/5'} ${scrolledUp && 'translate-y-[-150%] lg:translate-y-[0]'} rounded-[15px]`}>
+        <div className="relative flex flex-1 items-center justify-between lg:justify-start gap-2 mx-2 md:mx-4">
+          <div className="lg:hidden">
+            <Logo />
+          </div>
+          <Navigations />
+          <Searchbar scrolled={scrolled} />
         </div>
-        <Navigations scrolled={scrolled} />
-        <Searchbar />
       </div>
       <MusicPlayer scrolled={scrolled} />
     </div>
