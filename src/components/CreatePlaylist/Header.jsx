6df5c729e2057b73a6bde20b07c7fd090edfb861
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:51:22 +0100

    reordered back link and icon

--- src/components/CreatePlaylist/Header.jsx
+++ src/components/CreatePlaylist/Header.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom"
 
-import { MdChevronLeft } from "react-icons/md"
+import { MdArrowBack } from "react-icons/md"
 
 const Header = () => {
   return (
-    <div className="w-full flex justify-between items-center mb-6">
-      <h3 className="flex-1 text-white font-bold text-xl">Create new playlist</h3>
-      <Link to="/playlists" className='flex justify-center items-center hover:text-gray-100 text-gray-300 text-xs rounded-md'>
-        <span className="flex items-center justify-center"><MdChevronLeft size={15} /></span>
-        <span>Back</span>
+    <div className="w-full flex items-center gap-3 mb-6">
+      <Link to="/playlists" className='mt-[1px] flex justify-center items-center hover:text-gray-100 text-gray-300 text-xs rounded-md'>
+        <MdArrowBack size={25} />
       </Link>
+      <h3 className="flex-1 text-white font-bold text-xl">Create new playlist</h3>
     </div>
   )
 }
