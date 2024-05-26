80009d2c7689d2ee3df3953ed4369d106ffe9165
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:36:03 +0100

    Modified filter logic

--- src/components/List/Playlists.jsx
+++ src/components/List/Playlists.jsx
@@ -3,10 +3,10 @@ import { PlaylistCard } from '../Cards/'
 const Playlists = ({ playlistid, playlists }) => {
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-6 md:gap-4 gap-2">
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
       {
         playlists
-          .filter( playlist => playlist.id !== playlistid )
+          .filter( playlist => playlist.id != playlistid )
           .map( 
             (playlist, i) => <PlaylistCard i={i} playlist={playlist} key={i} /> 
           )
