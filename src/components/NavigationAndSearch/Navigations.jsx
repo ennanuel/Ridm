557572869b02735840d00f3b3163c8d011701af0
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:21:02 +0100

    Made design modifications to the buttons and stopped using 'scrolled' argument

--- src/components/NavigationAndSearch/Navigations.jsx
+++ src/components/NavigationAndSearch/Navigations.jsx
@@ -2,14 +2,15 @@ import { useMemo } from 'react';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 
-const Navigations = ({ scrolled }) => {
+const Navigations = () => {
   const navigate = useNavigate();
   const goBack = useMemo(() => () => navigate(-1), []);
   const goFront = useMemo(() => () => navigate(+1), []);
+  
   return (
     <>
-    <button onClick={goBack} className={`h-[40px] aspect-square hidden lg:flex items-center justify-center rounded-[20px] text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 ${scrolled ? 'bg-white/5' : 'bg-black/80'}`}><BsChevronLeft size={20} /></button>
-    <button onClick={goFront} className={`h-[40px] aspect-square hidden lg:flex items-center justify-center rounded-[20px] text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 ${scrolled ? 'bg-white/5' : 'bg-black/80'}`}><BsChevronRight size={20} /></button>
+      <button onClick={goBack} className={`h-[40px] aspect-square hidden lg:flex items-center justify-center rounded-[20px] text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 bg-black/50`}><BsChevronLeft size={20} /></button>
+      <button onClick={goFront} className={`h-[40px] aspect-square hidden lg:flex items-center justify-center rounded-[20px] text-gray-200 transition-transform active:scale-90 opacity-80 hover:opacity-100 bg-black/50`}><BsChevronRight size={20} /></button>
     </>
   )
 }
