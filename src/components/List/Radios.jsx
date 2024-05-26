4846b44a317ad748e5ba775a3fad342046fec636
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 18:35:25 +0100

    refactored code, modified some designs and stopped using 'favorites' and 'blacklist' arguments

--- src/components/List/Radios.jsx
+++ src/components/List/Radios.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import RadioCard from '../Cards/RadioCard';
 import { RadioLoading, Error } from '../LoadersAndError';
 import { getData } from '../../utils/getData';
@@ -8,37 +8,32 @@ import { useSelector } from 'react-redux';
 
 const Radios = ({ radios, children, showmore, isFetching, error, genreid, noFilter }) => {
     const { library } = useSelector(state => state);
-    const [newRadios, setNewRadios] = useState([]);
-
-    useEffect(() => {
-        const radiosData = getData({ type: 'radios', data: radios, noFilter })
-        setNewRadios(radiosData);
-    }, [library, radios, noFilter])
+    const newRadios = useMemo(() => getData({ type: 'radios', data: radios, noFilter }), [library, radios, noFilter]);
 
     return (
-        <div>
-        <div className="flex flex-row justify-between items-end mb-4">
-            <h3 className="text-white font-bold text-xl">{children}</h3>
-            {
-                showmore && <SeeMore link={`/charts?type=radios&genre=${genreid}`} />
-            }
-        </div>
-        {
-            isFetching ?
-            <RadioLoading num={5} /> :
-            error ?
-            <Error title="Could not load radios." /> :
-            !isFetching && !error && newRadios?.length < 1 ?
-            <Error title="Nothing was found" /> :
-            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-8">
+        <div id="radios">
+            <div className="flex flex-row justify-between items-end mb-4">
+                <h3 className="text-white font-bold text-xl">{children}</h3>
                 {
-                    newRadios?.map(
-                        (radio, i) => <RadioCard key={i} radio={radio} i={i} />
-                    )
+                    showmore && <SeeMore link={`/charts?type=radios&genre=${genreid}`} />
                 }
             </div>
-        }
-        </div>     
+            {
+                isFetching ?
+                    <RadioLoading num={5} /> :
+                    error ?
+                        <Error title="Could not load radios." /> :
+                        !isFetching && !error && newRadios?.length < 1 ?
+                            <Error title="Nothing was found" /> :
+                            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-8">
+                                {
+                                    newRadios?.map(
+                                        (radio, i) => <RadioCard key={i} radio={radio} i={i} />
+                                    )
+                                }
+                            </div>
+            }
+        </div>
     )
 }
 
