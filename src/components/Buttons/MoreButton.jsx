0eae6143d87e8f2e0274e2819f82c2de9c6405e3
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 19:12:33 +0100

    made minor style changes

--- src/components/Buttons/MoreButton.jsx
+++ src/components/Buttons/MoreButton.jsx
@@ -4,7 +4,7 @@ const MoreButton = ({ setLimit, limit, length }) => {
   return (
     (limit && length > 20) &&
     <div className="w-full flex justify-center my-[50px]">
-      <button onClick={() => setLimit(false)} className="flex items-center justify-center text-xs md:text-sm text-black bg-gray-200 border border-gray-200 rounded-[16px] h-[28px] md:h-[32px] font-semibold px-3 md:px-4 m-auto transition-[background-color] hover:bg-transparent hover:text-gray-200">
+      <button onClick={() => setLimit(false)} className="flex items-center justify-center text-xs md:text-sm text-black bg-gray-200 border border-white/5 rounded-[20px] h-[30px] md:h-[40px] font-semibold px-3 md:px-4 m-auto transition-[background-color] hover:bg-white/5 hover:text-gray-200">
         See more
       </button>
     </div>
