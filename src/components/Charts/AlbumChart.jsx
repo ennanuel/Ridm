9b3d68db6b68b160dce72f6517212454e4baf135
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 19:02:26 +0100

    refactored code, and stopped passing 'favorites' and 'blacklist' as parameters for 'Albums' component

--- src/components/Charts/AlbumChart.jsx
+++ src/components/Charts/AlbumChart.jsx
@@ -5,15 +5,15 @@ import { MoreButton } from '../Buttons'
 import { useSearchParams } from 'react-router-dom'
 import { useState } from 'react'
 
-const AlbumChart = ({ blacklist, favorites }) => {
+const AlbumChart = () => {
   const [limit, setLimit] = useState(true)
   const [params, setParams] = useSearchParams()
   const {data, isFetching, error} = useGetTopChartAlbumsQuery( params.get('genre') )
 
   return (
     <>
-    <Albums isFetching={isFetching} error={error} albums={data?.data?.slice(0, limit ? 20 : 50)} blacklist={blacklist} favorites={favorites} />
-    <MoreButton limit={limit} setLimit={setLimit} />
+      <Albums isFetching={isFetching} error={error} albums={data?.data?.slice(0, limit ? 20 : 50)} />
+      <MoreButton limit={limit} setLimit={setLimit} />
     </>
   )
 }
