a33102b08df6784b553199c78186473ac4d7c1e9
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-26 19:05:35 +0100

    modified styling of the card

--- src/components/Cards/GenreCard.jsx
+++ src/components/Cards/GenreCard.jsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom"
 
 const GenreCard = ({ genre, i }) => {
   return (
-    <Link to={'/genres/' + genre.id}>
-        <div style={{'--delay': i/10 + 's'}} className="genre-card relative rounded-md overflow-hidden w-full bg-white/5 transition-[background-color,transform] hover:bg-white/10 cursor-pointer">
-            <img src={genre.picture_medium} alt={genre.name} />
-            <div className="absolute bottom-0 left-0 px-4 w-full h-full flex flex-col items-end justify-end transition-colors bg-black/60 hover:bg-black/80">
-            <p className="text-gray-300 font-bold text-xs text-right">editorial</p>
-            <p className="text-white font-semibold text-base uppercase text-lg text-right">{genre.name}</p>
-            </div>
-        </div>
+    <Link
+      to={'/genres/' + genre.id}
+      style={{ '--delay': i / 10 + 's' }}
+      className="genre-card relative p-2 hover:bg-white/5 w-full flex flex-col gap-2 transition-[background-color,transform] rounded-lg aspect-square"
+    >
+      <img src={genre.picture_medium} alt={genre.name} className="rounded-xl" />
+      <div className="flex flex-col items-end justify-end transition-colors">
+        <p className="text-gray-300 font-bold text-xs text-right">editorial</p>
+        <p className="text-white font-semibold text-base uppercase text-right">{genre.name}</p>
+      </div>
     </Link>
-  )
+  );
 }
 
 export default GenreCard
