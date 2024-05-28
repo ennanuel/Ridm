2432cba28a3a9ab788a28e390336b442da92cc59
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 19:39:25 +0100

    Refactored code and imported new component

--- src/components/LoadersAndError/index.js
+++ src/components/LoadersAndError/index.js
@@ -1,11 +1,23 @@
-import Loader from './Loader'
-import Error from './Error'
-import AlbumLoading from './AlbumLoading'
-import ArtistLoading from './ArtistLoading'
-import SongLoading from './SongLoading'
-import LyricLoading from './LyricLoading'
-import RadioLoading from './RadioLoading'
-import GenreLoading from './GenreLoading'
-import HeaderLoading from './HeaderLoading'
+import Loader from './Loader';
+import Error from './Error';
+import AlbumLoading from './AlbumLoading';
+import TracksLoading from './TracksLoading';
+import ArtistLoading from './ArtistLoading';
+import SongLoading from './SongLoading';
+import LyricLoading from './LyricLoading';
+import RadioLoading from './RadioLoading';
+import GenreLoading from './GenreLoading';
+import HeaderLoading from './HeaderLoading';
 
-export { Loader, Error, AlbumLoading, SongLoading, ArtistLoading, LyricLoading, RadioLoading, GenreLoading, HeaderLoading }
+export {
+    Loader,
+    Error,
+    AlbumLoading,
+    TracksLoading,
+    SongLoading,
+    ArtistLoading,
+    LyricLoading,
+    RadioLoading,
+    GenreLoading,
+    HeaderLoading
+}
