e344c8042b488e47d3149d5bab17a83888113111
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-29 06:31:48 +0100

    redesigned components

--- src/components/Headers/PlaylistDetailsHeader/EditPlaylistDetails.jsx
+++ src/components/Headers/PlaylistDetailsHeader/EditPlaylistDetails.jsx
@@ -2,9 +2,9 @@ import { AiOutlineDelete } from "react-icons/ai"
 
 const EditPlaylistDetails = ({ editData, playlist, handleChange, songsToBeDeleted, handleEdit, setParams, handleDelete }) => {
   return (
-  <div className="flex-col flex-1">
+  <div className="relative z-[1] flex-col flex-1">
     <input 
-      className="px-2 py-1 rounded-md text-white text-base md:text-xl lg:text-2xl bg-white/10 focus:outline-none block w-fit placeholder:text-gray-400" 
+      className="px-2 h-[40px] md:h-[50px] rounded-[25px] text-white text-base bg-white/10 focus:outline-none block w-fit placeholder:text-gray-400" 
       value={editData.name} 
       placeholder={playlist.name} 
       onChange={handleChange} 
@@ -16,13 +16,13 @@ const EditPlaylistDetails = ({ editData, playlist, handleChange, songsToBeDelete
 
     <div className="flex flex-row gap-4 flex-wrap justify-between items-center">
       <div className="flex items-center gap-2">
-        <button onClick={handleEdit} className="py-1 md:py-2 px-3 md:px-4 font-bold transition-colors bg-gray-300 border-2 border-gray-300 hover:border-gray-100 hover:bg-gray-100 text-black rounded-md">Done</button>
-        <button onClick={() => setParams({})} className="py-1 md:py-2 px-3 md:px-4 font-bold transition-colors border-2 border-300 hover:border-gray-100 hover:text-gray-100 text-gray-300 rounded-md">Cancel</button>
+        <button onClick={handleEdit} className="px-3 md:px-4 h-[36px] md:h-[46px] rounded-[25px] font-bold transition-colors bg-gray-300 hover:bg-white border border-gray-300 hover:border-gray-100 text-black">Done</button>
+        <button onClick={() => setParams({})} className="px-3 md:px-4 h-[36px] md:h-[46px] rounded-[25px] font-bold transition-colors bg-white/5 hover:bg-white/10 border border-300 hover:border-gray-100 hover:text-white text-gray-300">Cancel</button>
       </div>
       {
         songsToBeDeleted.length > 0 &&
-        <button onClick={handleDelete} className="flex items-center justify-center bg-black/80 text-red-500 p-1 md:p-2 rounded-md">
-          <AiOutlineDelete size={30} />
+        <button onClick={handleDelete} className="flex items-center justify-center bg-black/80 text-red-500 h-[40px] w-[40px] rounded-[10px]">
+          <AiOutlineDelete size={25} />
         </button>
       }
     </div>
