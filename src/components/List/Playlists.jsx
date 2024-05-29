8e4f5b9f3b6ea5b4e63fe3520a82cb90bf5215b3
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-29 05:53:05 +0100

    stopped filtering playlists

--- src/components/List/Playlists.jsx
+++ src/components/List/Playlists.jsx
@@ -1,12 +1,11 @@
 import { PlaylistCard } from '../Cards/'
 
-const Playlists = ({ playlistid, playlists }) => {
+const Playlists = ({ playlists }) => {
 
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
       {
         playlists
-          .filter( playlist => playlist.id != playlistid )
           .map( 
             (playlist, i) => <PlaylistCard i={i} playlist={playlist} key={i} /> 
           )
