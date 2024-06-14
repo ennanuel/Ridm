1b727f1dc4f5223ceb12bae682e2c59c6bd579d1
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-06-14 09:29:28 +0100

    Added serveless functions

---
+++ netlify/functions/musixmatch/index.mjs
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+async function musixMatchHandler(req) {
+    try {
+        const endpoint = req.params;
+        const params = { ...req.query, apikey: process.env.MUSIXMATCH_API_KEY };
+
+        const URL = `${process.env.MUSIXMATCH_URL}/${endpoint}`;
+
+        const response = await axios.get(URL, { params });
+
+        console.log("API call successful!");
+        
+        return {
+            body: response.data,
+            statusCode: 200
+        }
+    } catch (error) {
+        console.error(error);
+        return new Response(error.message, { status: 500 });
+    }
+};
+
+export default musixMatchHandler;
+
+export const config = {
+    path: "/lyrics/:endpoint([\\/\\w\\.-]*"
+};
