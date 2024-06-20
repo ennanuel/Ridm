eca1fa7d2b22c77900f8e77ecbb47d5c08f14c81
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-06-20 16:58:59 +0100

    added functions again

---
+++ netlify/functions/musixmatch/index.mjs
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+async function musixMatchHandler(req) {
+    try {
+        const { path, ...query } = req.query;
+        const params = { ...query, apikey: process.env.MUSIXMATCH_API_KEY };
+
+        const URL = `${process.env.MUSIXMATCH_URL}/${path}`;
+
+        const response = await axios.get(URL, { params });
+        
+        return Response.json(response.data, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return new Response(error.message, { status: 500 });
+    }
+};
+
+export default musixMatchHandler;
+
+export const config = {
+    path: "/api/lyrics"
+};
