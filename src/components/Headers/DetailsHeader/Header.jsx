da840ee093799653c20bafc26eea73cdaf2c2308
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-05-10 12:11:17 +0100

    Modified the header title

--- src/components/Headers/DetailsHeader/Header.jsx
+++ src/components/Headers/DetailsHeader/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({ type, title, text }) => {
             <p style={{color: text}} className="font-bold text-xs md:text-sm text-gray-400 relative ml-5 md:mb-2 opacity-80">
             { type }
             </p>
-        <h2 style={{color: text}} className="text-3xl leading-[4rem] md:text-[4rem] text-white font-bold relative mx-4 md:mb-4 break-words">
+        <h2 style={{color: text}} className="text-3xl leading-[2.25rem] md:leading-[4rem] md:text-[4rem] text-white font-bold relative mx-4 md:mb-4 break-words">
             { headerTitle }
         </h2>
         </>
