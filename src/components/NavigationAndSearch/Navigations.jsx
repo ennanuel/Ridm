c287ee6cd4e653ecc210d393a978d1bfd89fed46
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-21 23:26:57 +0100

    Made search and navigation buttons opaque

--- src/components/NavigationAndSearch/Navigations.jsx
+++ src/components/NavigationAndSearch/Navigations.jsx
@@ -11,13 +11,13 @@ const Navigations = () => {
     <div className='flex items-center gap-2'>
       <button 
         onClick={goBack} 
-        className={`h-8 aspect-square hidden lg:flex items-center justify-center rounded-full text-gray-300 transition-transform active:scale-90 hover:text-white bg-black/50`}
+        className={`h-8 aspect-square hidden lg:flex items-center justify-center rounded-full text-gray-300 transition-transform active:scale-90 hover:text-white bg-zinc-800/80`}
       >
         <FiChevronLeft size={20} />
       </button>
       <button 
         onClick={goFront} 
-        className={`h-8 aspect-square hidden lg:flex items-center justify-center rounded-full text-gray-300 transition-transform active:scale-90 hover:text-white bg-black/50`}
+        className={`h-8 aspect-square hidden lg:flex items-center justify-center rounded-full text-gray-300 transition-transform active:scale-90 hover:text-white bg-zinc-800/80`}
       >
         <FiChevronRight size={20} />
       </button>
