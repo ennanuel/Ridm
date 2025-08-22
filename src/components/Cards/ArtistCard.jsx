e465224e9b0702df5ad91756bf44239994de54d3
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-22 14:05:18 +0100

    Editted border-radius

--- src/components/Cards/ArtistCard.jsx
+++ src/components/Cards/ArtistCard.jsx
@@ -4,7 +4,7 @@ const ArtistCard = ({ artist, i }) => {
   const navigate = useNavigate()
   
   return (
-    <div style={{'--delay': i/20 + 's'}} className="artist-card flex flex-col p-2 md:p-3 rounded-xl cursor-pointer transition-[background-color] hover:bg-white/5" onClick={() => { navigate(`/artists/${artist.id}`) }}>
+    <div style={{'--delay': i/20 + 's'}} className="artist-card flex flex-col p-2 md:p-3 rounded-md cursor-pointer transition-[background-color] hover:bg-white/5" onClick={() => { navigate(`/artists/${artist.id}`) }}>
       <img alt="artist" src={artist?.picture_medium} className="w-full aspect-square rounded-full" />
       <p className="mt-3 font-bold text-[0.7em] text-gray-400 text-left truncate">{artist.type}</p>
       <p className="font-semibold text-sm text-white text-left truncate">{artist.name}</p>
