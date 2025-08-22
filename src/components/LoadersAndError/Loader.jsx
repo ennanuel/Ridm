76252c5abc960506cd14f2dbf96decc3d772a99b
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-22 19:58:22 +0100

    Refactored code

--- src/components/LoadersAndError/Loader.jsx
+++ src/components/LoadersAndError/Loader.jsx
@@ -1,12 +1,12 @@
 import { loader } from '../../assets/images'
 
-const Loader = ({ title }) => (
-  <div className="w-full flex-justify-center items-center flex-col">
-    <img src={loader} alt="loader" className='h-[100px] w-[100px] object-contain block m-auto' />
-    <h1 className="font-bold text-base text-center text-gray-200 mt-2">
-      {title}
-    </h1>
-  </div>
-);
-
-export default Loader;
+export default function ({ title }) {
+  return (
+    <div className="w-full flex-justify-center items-center flex-col">
+      <img src={loader} alt="loader" className='h-[100px] w-[100px] object-contain block m-auto' />
+      <h1 className="font-bold text-base text-center text-gray-200 mt-2">
+        {title}
+      </h1>
+    </div>
+  )
+};
