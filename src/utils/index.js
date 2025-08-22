ff4eb3a7783c8eb54232555b738debf5305ab9a9
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-22 20:00:52 +0100

    refactored code to fix potential bug

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,9 @@
 export function generateArray(size) {
     if (!size || typeof (size) !== 'number' || size < 1) return [];
     const values = [];
-    for (let i = size; i > 0 && values.length <= size; i--) `${values.push(Math.ceil(Math.random() * 100))}%`;
+    for (let i = size; i > 0 && values.length <= size; i--) {
+        const value = `${Math.ceil(Math.random() * 100)}%`;
+        values.push(value);
+    }
     return values;
 }
