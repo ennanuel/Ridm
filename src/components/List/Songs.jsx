061c725dbacfe2c0b4238c9c5cf00df6a2f83cb7
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-22 14:04:37 +0100

    Refactored component, and modified the design a little

--- src/components/List/Songs.jsx
+++ src/components/List/Songs.jsx
@@ -2,37 +2,26 @@ import { useMemo } from "react";
 
 import { SongBar } from "../Cards";
 import { SongLoading, Error } from "../LoadersAndError";
-import SeeMore from "./SeeMore"
+import SeeMore from "./SeeMore";
 
 import { getData } from "../../utils/getData";
 import { useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const Songs = ({ songs, suggestion, children, isFetching, error, showmore, genreid, noFilter, full, bg }) => {
+export default function Songs ({ songs, suggestion, children, isFetching, error, showmore, genreid, noFilter, full, bg }) {
   const library = useSelector(state => state.library);
   const [params] = useSearchParams();
 
   const tracks = useMemo(() => getData({ type: 'tracks', data: songs, noFilter, sortType: params.get('sort') }), [library, songs, noFilter]);
-
-  const background = useMemo(() =>  (full && bg) && bg.replace(')', ',0.6)'), [bg, full, songs]);
+  const parentClassName = useMemo(() => `relative flex flex-col ${(!full && !suggestion) && 'mb-4'} ${(full && !suggestion) && 'p-3 md:p-4 lg:p-6 rounded-xl overflow-clip'} ${bg ? 'bg-zinc-900 border border-white/5' : ''}`, [full, suggestion, bg]);
 
   return (
-    <div id='tracks' className={`relative flex flex-col ${(!full && !suggestion) && 'mb-4'} ${(full && !suggestion) && 'p-3 md:p-4 lg:p-6 rounded-xl overflow-clip'} ${bg && 'bg-zinc-900'}`}>
-      {
-        (full && bg) &&
-        <div style={{ background }} className="absolute z-[0] opacity-60 w-full h-full top-0 left-0"></div>
-      }
-      <div className={`relative z-1 flex flex-row justify-between items-end ${isFetching && 'mb-4'} ${suggestion ? 'lg:hidden' : ''}`}>
-        {
-          children && isFetching ?
-            <span className="h-6 rounded-md w-full max-w-[240px] bg-white/5 animation-loading"></span> :
-            <h3 className="text-white/80 font-bold text-xl sm:text-2xl mb-6">{children}</h3>
-        }
-        {
-          showmore &&
-          <SeeMore link={`/charts?type=songs&genre=${genreid}`} />
-        }
-      </div>
+    <div 
+      id='tracks' 
+      className={parentClassName}
+     >
+      <Background showBackground={full && bg} backgroundColor={bg} />
+      <TitleAndSeeMore children={children} isFetching={isFetching} suggestion={suggestion} showMore={showmore} genreId={genreid} />
       {
         isFetching ?
           <SongLoading num={4} full={full} /> :
@@ -56,4 +45,27 @@ const Songs = ({ songs, suggestion, children, isFetching, error, showmore, genre
   )
 };
 
-export default Songs;
+function TitleAndSeeMore({ isFetching, children, showMore, suggestion, genreId }) {
+  return (
+      <div className={`relative z-1 flex flex-row justify-between items-end ${isFetching ? 'mb-4' : ''} ${suggestion ? 'lg:hidden' : ''}`}>
+        {
+          children && isFetching ?
+            <span className="h-6 rounded-md w-full max-w-[240px] bg-white/5 animation-loading"></span> :
+            <h3 className="text-white/80 font-bold text-xl sm:text-2xl mb-6">{children}</h3>
+        }
+        {
+          showMore ?
+            <SeeMore link={`/charts?type=songs&genre=${genreId}`} /> :
+            null
+        }
+      </div>
+  )
+};
+
+function Background({ showBackground, backgroundColor }) {
+  if(!showBackground) return;
+
+  return (
+    <div style={{ backgroundColor }} className="absolute z-[0] opacity-50 w-full max-w-[400px] h-full min-h-[320px] max-h-[400px] -top-1/2 left-1/2 -translate-x-1/2 rounded-[50%] blur-[64px]" />
+  )
+};
